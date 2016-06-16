2783af6fb3bee2819c31b917a05aeda71947c71b
Spencer Krum <nibz@spencerkrum.com>
2016-06-15 20:46:07 -0700

    Functional jquery and GET requests
    This just uses an anonymous github api for example fun

--- vinz2.user.js
+++ vinz2.user.js
@@ -5,26 +5,30 @@
 // @include     https://review.openstack.org/#/c/164357*
 // @version     1
 // @grant       none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
 
 //alert("hello world from vim");
-$( "#gerrit_header" ).html("");
-$( "body" ).html("");
+$( "#gerrit_header" ).remove();
 $( window ).load(function() { 
     // Handler for .ready() called.
     console.log('sup2');
     $("#gerrit_ui").remove();
     $("#toggleci").remove();
-  var items = [];
-  data = [1,2,3,4];
-  $.each( data, function( index ) {
-    items.push( "<li id='" + data[index] + "'>" + data[index] + "</li>" );
+
+ $.getJSON( "https://api.github.com/users/voxpupuli/repos", function( data ) {
+    var items = [];
+    $.each( data, function( index ) {
+      items.push( "<li id='" + data[index].name + "'>" + data[index].name + "</li>" );
+    });
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+        alert( "Load was performed." );
   });
 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
 
 });
+$( "body" ).html("");
