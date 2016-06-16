8b116e2e174409eae39c393a4631750ee8aaf078
Spencer Krum <nibz@spencerkrum.com>
2016-06-15 22:31:17 -0700

    Functionally dumping 'files changed'

--- vinz2.user.js
+++ vinz2.user.js
@@ -7,26 +7,40 @@
 // @grant       none
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
+//
+
+function gerritGet (url, func){
+  var ret;
+  $.get( get_url, function( data ) {
+  })
+  .fail(function(data) {
+    ret = jQuery.parseJSON(data.responseText.slice(4));
+    func(ret);
+  });
+}
+
 
 
 //alert("hello world from vim");
 $( "#gerrit_header" ).remove();
-$( window ).load(function() { 
+var get_url = "https://review.openstack.org/changes/330250/revisions/09ee4176e7394e0c29ca3fe948033d8ebae230ec/files";
+$( document ).ready(function() {
     // Handler for .ready() called.
     console.log('sup2');
     $("#gerrit_ui").remove();
     $("#toggleci").remove();
+    $( "body" ).add("<p>").text("Files changed: ");
 
- $.getJSON( "https://api.github.com/users/voxpupuli/repos", function( data ) {
+  gerritGet( get_url, function (data) {
     var items = [];
     $.each( data, function( index ) {
-      items.push( "<li id='" + data[index].name + "'>" + data[index].name + "</li>" );
+      items.push( "<li id='" + index + "'>" + index + "</li>" );
     });
     $( "<ul/>", {
-      "class": "my-new-list",
+      "class": "files-modified-list",
       html: items.join( "" )
     }).appendTo( "body" );
-        alert( "Load was performed." );
+        //console.log( "Load was performed." );
   });
 
 
