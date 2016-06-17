d337ad2d876b00221b13c1d847ef43f84cbf3125
Spencer Krum <nibz@spencerkrum.com>
2016-06-17 00:26:06 -0700

    Add bootstrap headers

--- vinz.user.js
+++ vinz.user.js
@@ -29,6 +29,11 @@ $( document ).ready(function() {
   $( "#gerrit_ui").remove();
   $( "#toggleci").remove();
 
+  // bootstrap
+  $( "head" ).prepend('<meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->');
+  $( "head" ).append('<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">');
+  $( "head" ).append('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>');
+
   // Get auth Token
   $.get("/", function (data) {
     console.log("foo");
